e08caaa88b6bf891f6610d08d7e4b7fb79801384
piero lavado cervantes <piero_lvdo_c@hotmail.com>
2020-10-31 03:32:46 -0500

    finalizacion pantalla detalles

--- lib/src/pages/detalle_habitacion.dart
+++ lib/src/pages/detalle_habitacion.dart
@@ -1,6 +1,7 @@
 
 import 'package:componentes/src/models/detalle_hab_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class DetalleHab extends StatelessWidget {
 
@@ -11,7 +12,7 @@ class DetalleHab extends StatelessWidget {
   Widget build(BuildContext context) {
 
     detalleH.habitacionTitulo = "Habitacion 301";
-    detalleH.descripcion = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.";
+    detalleH.descripcion = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500.";
 
     return Scaffold(
       body: CustomScrollView(
@@ -21,11 +22,13 @@ class DetalleHab extends StatelessWidget {
             delegate: SliverChildListDelegate(
               [
                 SizedBox( height: 10.0),
+                _posterTitulo( context, detalleH ),
+                _caracteristicasG( detalleH ),
                 _descripcion( detalleH ),
-                _descripcion( detalleH ),
-                _descripcion( detalleH ),
-                _descripcion( detalleH ),
-                _descripcion( detalleH )
+                SizedBox( height: 20.0),
+                _tarjetasHabitaciones(),
+                SizedBox( height: 20.0),
+                _botonPagar(),
               ]
             ),
           ),
@@ -39,14 +42,14 @@ class DetalleHab extends StatelessWidget {
     return SliverAppBar(
       elevation: 2.0,
       backgroundColor: Colors.indigoAccent,
-      expandedHeight: 200.0,
+      expandedHeight: 180.0,
       floating: false,
       pinned: true,
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
         title: Text(
           det.habitacionTitulo,
-          style: TextStyle( color: Colors.white, fontSize: 16.0),
+          style: TextStyle( color: Colors.white, fontSize: 18.0),
         ),
         background: FadeInImage(
           image: NetworkImage( "https://cf.bstatic.com/images/hotel/max1024x768/188/188154647.jpg" ),//Deberia estar registrada la imagen en algun lugar
@@ -62,12 +65,145 @@ class DetalleHab extends StatelessWidget {
   Widget _descripcion( DetalleHabitacion det ) {
 
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: Text(
         det.descripcion,
-        textAlign: TextAlign.justify,
+        style: TextStyle( fontSize: 12.0 ), textAlign: TextAlign.justify
       ),
     );
 
+  }
+
+  Widget _caracteristicasG( DetalleHabitacion det ) {
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Características generales", style: TextStyle( fontSize: 16.0 , fontWeight: FontWeight.bold) , overflow: TextOverflow.ellipsis,),
+          SizedBox( height: 10.0,),
+          _barraIconos()
+        ],
+      ),
+    );
+
+  }
+
+  Widget _posterTitulo( BuildContext context, DetalleHabitacion detHab ){
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image(
+                image: NetworkImage( "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQAItQPrAGjli0mKCMkr5EPbEj1coK5ATHs1Q&usqp=CAU" ),
+                height: 150.0,
+              ),
+          ),
+          SizedBox(width: 20.0,),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(detHab.habitacionTitulo, style: Theme.of(context).textTheme.headline6, overflow: TextOverflow.ellipsis,),
+                SizedBox( height: 5.0),
+                Text(detHab.descripcion, style: TextStyle( fontSize: 12.0 ), textAlign: TextAlign.justify,),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+
+  }
+
+  Widget _barraIconos(){
+
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 3),
+        borderRadius: BorderRadius.all(
+            Radius.circular(10.0)
+        )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Icon( Icons.wifi , size: 40.0),
+          ),
+          SizedBox( width: 25.0,),
+          Container(
+            child: Icon( Icons.traffic , size: 40.0),
+          ),
+          SizedBox( width: 25.0,),
+          Container(
+            child: Icon( Icons.computer , size: 40.0),
+          ),
+          SizedBox( width: 25.0,),
+          Container(
+            child: Icon( Icons.contact_phone , size: 40.0),
+          ),
+          SizedBox( width: 25.0,),
+          Container(
+            child: Icon( Icons.credit_card , size: 40.0),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _tarjetasHabitaciones(){
+    /*return Expanded(
+      child: Container(
+        height: 200,
+        child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return Image.network(
+              "https://webbox.imgix.net/images/tkzintxirlbyxpfq/aab785f4-e980-4f6f-a6b3-85c2cf5743d3.jpg?auto=format,compress&fit=crop&crop=entropy&w=600&h=450",
+              fit: BoxFit.fitHeight,
+            );
+          },
+          itemCount: 5,
+          itemWidth: 300.0,
+          layout: SwiperLayout.STACK,
+        ),
+      )
+    );*/
+
+    return Container(
+        height: 200,
+        child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return Image.network(
+              "https://webbox.imgix.net/images/tkzintxirlbyxpfq/aab785f4-e980-4f6f-a6b3-85c2cf5743d3.jpg?auto=format,compress&fit=crop&crop=entropy&w=600&h=450",
+              fit: BoxFit.fitHeight,
+            );
+          },
+          itemCount: 5,
+          itemWidth: 300.0,
+          layout: SwiperLayout.STACK,
+        ),
+      );
+  }
+
+  Widget _botonPagar(){
+    return Center(
+      child: Container(
+        width: 300.0,
+        height: 50.0,
+        child: RaisedButton(
+              child: Text("Reservar por S/.240"),
+              color: Colors.blue,
+              textColor: Colors.white,
+              shape: StadiumBorder(),
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              onPressed: () {},
+            ),
+      ),
+    );
   }
 }
